5652f2190d4a72b45fff0b8062886c1b9325c720
Daniel Kwittung <d.kwittung@sprinteins.com>
2020-10-14 13:17:15 +0200

    progress progress progress

---
+++ humhub-1.6.4/themes/trnbtl-theme/views/activity/views/layouts/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+use humhub\components\ActiveRecord;
+use humhub\modules\content\components\ContentContainerController;
+use humhub\modules\space\models\Space;
+use humhub\modules\space\widgets\Image;
+use humhub\widgets\TimeAgo;
+use yii\helpers\Url;
+
+/* @var $originator \humhub\modules\user\models\User  */
+/* @var $clickable boolean  */
+/* @var $record ActiveRecord  */
+
+?>
+
+<?php if ($clickable) : ?>
+<a href="<?= Url::to(['/activity/link', 'id' => $record->id])?>">
+<?php endif; ?>
+    <li class="activity-entry" data-stream-entry data-action-component="activity.ActivityStreamEntry" data-content-key="<?= $record->content->id ?>">
+        <div class="media">
+            <?php if ($originator !== null) : ?>
+                <!-- Show user image -->
+                <?= $originator->getProfileImage()->render(32, ['class' => 'media-object', 'link' => false, 'htmlOptions' => ['class' => 'pull-left']]) ?>
+            <?php endif; ?>
+
+            <div class="media-body text-break">
+
+                <!-- Show content -->
+                <?= $content ?>
+                <br>
+
+                <!-- show time -->
+                <?= TimeAgo::widget(['timestamp' => $record->content->created_at]) ?>
+            </div>
+        </div>
+    </li>
+<?php if ($clickable) : ?>
+</a>
+<?php endif; ?>
